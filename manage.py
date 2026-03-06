97609c200d2773acdc7cc754c4561c95f8d80510
crclaims <eg.claimsresolution@gmail.com>
2026-03-06 17:51:43 -0500

    Update manage.py

--- manage.py
+++ manage.py
@@ -6,22 +6,24 @@ app = create_app()
 
 with app.app_context():
     db.create_all()
-    temp_password = os.getenv("ADMIN_TEMP_PASSWORD", "2lau#NnQYR2qIojbKphS")
+    temp_password = os.getenv("ADMIN_TEMP_PASSWORD", "2lau#NnQYR2q1ojbKphS")
     users = [
         ("Elizabeth", "JD.claimsresolution@gmail.com", "admin"),
         ("Genesis", "genesis@local.crm", "canvasser"),
         ("Jesus", "jesus@local.crm", "caller"),
     ]
+
     for full_name, email, role in users:
-    u = User.query.filter_by(email=email).first()
-    if not u:
-        u = User(full_name=full_name, email=email, role=role)
-        db.session.add(u)
+        u = User.query.filter_by(email=email).first()
 
-    if email == "JD.claimsresolution@gmail.com":
-        u.full_name = full_name
-        u.role = "admin"
-        u.active = True
-        u.set_password(temp_password)
+        if not u:
+            u = User(full_name=full_name, email=email, role=role)
+            db.session.add(u)
 
-db.session.commit()
+        if email == "JD.claimsresolution@gmail.com":
+            u.full_name = full_name
+            u.role = "admin"
+            u.active = True
+            u.set_password(temp_password)
+
+    db.session.commit()
